ade2a0e3cc6b8e0f77fdea0cb0eaf4a221e9e7fb
Alexander Sinitsin <alexander.sinitsyn@it-grands.ru>
2017-01-07 16:59:30 +0700

    0.1.0 - Подключил шаблон Inspinia

---
+++ src/WebSite/MVC/Helpers/JsHelper.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Smik.WebSite.Mvc.Helpers
+{
+    public static class JsHelper
+    {
+        public static string Serialize(object o)
+        {
+            var serializeString = JsonConvert.SerializeObject(o);
+            return serializeString;
+        }
+    }
+}
---
+++ src/WebSite/MVC/Helpers/MenuHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Smik.WebSite.Mvc.Helpers
+{
+    public static class MenuHelper
+    {
+        public static string IsSelected(this HtmlHelper html, string actions = null, string cssClass = "active")
+        {
+            if (string.IsNullOrWhiteSpace(actions))
+            {
+                return cssClass;
+            }
+
+            var viewContext = html.ViewContext.IsChildAction ? html.ViewContext.ParentActionViewContext : html.ViewContext;
+            var currentAction = ((string)viewContext.RouteData.Values["action"]).ToLowerInvariant();
+            var currentController = ((string)viewContext.RouteData.Values["controller"]).ToLowerInvariant();
+
+            return actions.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(a =>
+            {
+                string controller = null;
+                string action = null;
+
+                var pair = a.Split('.').Select(s => s.Trim().ToLowerInvariant()).ToList();
+                if (pair.Count > 0)
+                {
+                    controller = pair[0];
+                }
+
+                if (pair.Count > 1)
+                {
+                    action = pair[1];
+                }
+
+                return new { Controller = controller, Action = action };
+            })
+            .Any(action => action.Controller == currentController && (action.Action == null || action.Action == currentAction))
+                ? cssClass
+                : string.Empty;
+        }
+    }
+}
