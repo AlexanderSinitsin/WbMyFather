944b50abb7c7afa36dfe17649dd759dee15ee29c
Alexander Sinitsin <alexander.sinitsyn@it-grands.ru>
2017-01-13 19:37:54 +0700

    0.1.0 - Поправил таблицу, добавил необходимые скрипты

---
+++ src/WebSite/Assets/js/common/extensions/ArrayExtensions.js
@@ -0,0 +1,10 @@
+﻿Array.prototype.contains = function (value) {
+  return this.indexOf(value) > -1;
+};
+
+Array.prototype.removeValue = function (value) {
+  var index = this.indexOf(value);
+  if (index > -1) {
+    this.splice(index, 1);
+  }
+};
--- src/WebSite/Assets/js/common/extensions/StringExtensions.js
+++ src/WebSite/Assets/js/common/extensions/StringExtensions.js
@@ -5,3 +5,12 @@
     return typeof value != 'undefined' ? value : match;
   });
 };
+
+String.format = function (format) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  return format.replace(/{(\d+)}/g, function (match, number) {
+    return typeof args[number] != 'undefined'
+      ? args[number]
+      : match;
+  });
+};
